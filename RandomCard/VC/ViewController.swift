3a630b911304def16ff806063e5ba9f7451237ce
EisenKim <daseisen@gmail.com>
2018-05-06 16:55:54 +0900

    3

--- RandomCard/VC/ViewController.swift
+++ RandomCard/VC/ViewController.swift
@@ -9,10 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var arrMeterials:[String] = ["해","달","별","양","염소","꽃","구름","창","칼","방패"]
+    var arrCards = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.arrCards = self.generateRandomCard()
+        
+        for i in 0 ..< 3 {
+            print(arrCards[i])
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func generateRandomCard() -> [String]  {
+        var arrResult = [String]()
+        while arrMeterials.count > 0 {
+            // random key from array
+            let arrayKey = Int(arc4random_uniform(UInt32(arrMeterials.count)))
+            // your random number
+            let randCard = arrMeterials[arrayKey]
+            arrResult.append(randCard)
+            // make sure the number isnt repeated
+            arrMeterials.remove(at: arrayKey)
+        }
+        return arrResult
+    }
+    
 }
 
